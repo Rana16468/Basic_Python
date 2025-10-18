037c6fbfbfdaeafcd2c4aebcee21bb060148144e
Rana16468 <amsohelrana.me@gmail.com>
2025-10-18 06:31:19 +0600

    resolving two problem

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
-# just starting 
-a=10
-b=10
-c=a+b
-print('sum',c)
-print("a=>",a)
-print("b=>",b)
+# # just starting 
+# a=10
+# b=10
+# c=a+b
+# print('sum',c)
+# print("a=>",a)
+# print("b=>",b)
 
-print("====> output ===>")
+# print("====> output ===>")
 
-for i in range(5):
-    aa=i*i
-    print(aa)
+# for i in range(5):
+#     aa=i*i
+#     print(aa)
+
+# problem 1 
+a = 5
+b = 6
+
+if a > b:
+    print("a is large number", a)
+else:
+    print("b is large number", b)
+
+#problem 2
+n=int(input("Enter the any number: "))
+for number in range(1,n+1):
+    print(number)
 
